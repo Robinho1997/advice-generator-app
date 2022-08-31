8de3c8aa0adb9406e1e4f8201a29cb8dcb7d3b36
Robinho1997 <robin.kempen@gmx.de>
2022-08-31 18:06:01 +0200

    advice generator app

---
+++ script.js
@@ -0,0 +1,20 @@
+const adviceID = document.getElementById("advice-header");
+const adviceText = document.getElementById("advice-text");
+
+
+window.onload(getAdvice());
+
+
+function getAdvice() { 
+
+fetch("https://api.adviceslip.com/advice").then(response => {
+    return response.json();
+}).then(adviceData => {
+   const adviceObject = adviceData.slip;
+   adviceID.innerText = `Advice # ${adviceObject.id}`;
+   adviceText.innerText = adviceObject.advice;
+})
+
+}
+
+
